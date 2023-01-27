146eed41a0c5f4118876d2b720168fa5163251e1
71Arid <brian87noel@gmail.com>
2023-01-27 10:15:46 +0300

    new

--- convert_number.c
+++ convert_number.c
@@ -41,7 +41,7 @@ int print_HEX(va_list ap, params_t *params)
 	if (params->l_modifier)
 		l = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int)
+		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
 	str = convert(l, 16, CONVERT_UNSIGNED, params);
